abf1ddfb299ec7ec671db9f6d003649890bb1f32
lina <jack@jackli.dev>
2022-07-26 22:07:40 -0700

    feat: remove popup for normal qna

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -4,5 +4,8 @@ scrollHiddenElems.forEach(function (elem) {
     elem.classList.remove("cs-dm-add");
 });
 
-/* remove big device popup */
+/* remove big device popup - for textbook */
 document.getElementById("cs-dm-add")?.remove();
+
+/* remove big device popup - for normal qna */
+document.getElementById("device-management-registration-modal-container")?.remove();
