96f0fedb22927750521aae1cd78d080422d1204f
lina <jack@jackli.dev>
2022-07-26 21:50:09 -0700

    feat: remove popup

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,4 +1,8 @@
+/* remove overflow:hidden, allowing scrolling again */
 var scrollHiddenElems = document.querySelectorAll(".cs-dm-add");
 scrollHiddenElems.forEach(function (elem) {
     elem.classList.remove("cs-dm-add");
 });
+
+/* remove big device popup */
+document.getElementById("cs-dm-add")?.remove();
